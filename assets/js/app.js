62173db41298d50e413edc7a61c1299648f64f52
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-10-29 12:01:17 -0500

    Se agrega la imagen diaria de la nasa al proyecto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 console.log("Entro app.js");
 const description = document.getElementById("description");
-//const imagenNasa = document.getElementById("imagen_nasa");
+const imagenNasa = document.getElementById("imagen_nasa");
 
 const clave = "DEMO_KEY";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`)
@@ -8,13 +8,13 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`)
   .then(datosApi => {
     // Acceder a la descripción y URL de la imagen en los datos de la API
     const descripcion = datosApi.explanation;
-    //const imagenUrl = datosApi.url;
+    const imagenUrl = datosApi.url;
 
     // Asignar la descripción al elemento <p> en tu HTML
     description.textContent = descripcion;
 
     // Asignar la URL de la imagen al atributo src del elemento <img>
-    //imagenNasa.src = imagenUrl;
+    imagenNasa.src = imagenUrl;
   })
   .catch(error => {
     console.error("Error al obtener los datos de la API:", error);
