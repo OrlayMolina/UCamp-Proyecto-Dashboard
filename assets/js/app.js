b310f0e544433bd9444bd1fdc6c2a08f0c14d54a
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-10-28 21:23:35 -0500

    Se agregan la api de la nasa pero no nuestra la imagen, queda pendiente de revisión

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+console.log("Entro app.js");
+const description = document.getElementById("description");
+//const imagenNasa = document.getElementById("imagen_nasa");
+
+const clave = "DEMO_KEY";
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`)
+  .then(response => response.json())
+  .then(datosApi => {
+    // Acceder a la descripción y URL de la imagen en los datos de la API
+    const descripcion = datosApi.explanation;
+    //const imagenUrl = datosApi.url;
+
+    // Asignar la descripción al elemento <p> en tu HTML
+    description.textContent = descripcion;
+
+    // Asignar la URL de la imagen al atributo src del elemento <img>
+    //imagenNasa.src = imagenUrl;
+  })
+  .catch(error => {
+    console.error("Error al obtener los datos de la API:", error);
+  });
