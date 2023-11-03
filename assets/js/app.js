b84814ef70d41fda1d5d1924433b0dbf7fd70b79
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-11-03 14:52:49 -0500

    Se actualiza proyecto con los cambios hecho en la asesoria UCamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,12 @@ const imagenNasa = document.getElementById("imagen_nasa");
 const tbody = document.getElementById('tbody');
 
 const clave = "23Z86A9FIh6Cq1b8nfY6iov2BidVN7K2fk0iLNu9";
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`)
-  .then(response => response.json())
-  .then(datosApi => {
+
+async function fetchNasaData() {
+  try {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`);
+    const datosApi = await response.json();
+
     // Acceder a la descripción y URL de la imagen en los datos de la API de la NASA
     const descripcion = datosApi.explanation;
     const imagenUrl = datosApi.url;
@@ -16,38 +19,61 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${clave}`)
 
     // Asignar la URL de la imagen al atributo src del elemento <img>
     imagenNasa.src = imagenUrl;
-  })
-  .catch(error => {
+  } catch (error) {
     console.error("Error al obtener los datos de la API de la NASA:", error);
-  });
+  }
+}
+
+fetchNasaData();
+
 
 // Obtener datos de los 10 primeros asteroides
 fetch("https://ssd-api.jpl.nasa.gov/sb_sat.api", {
-  method: 'GET'
+  method: 'GET',
+  mode: 'cors'
 })
   .then(response => response.json())
   .then(datos => {
     const ctx = document.getElementById('myChart');
+    const tbody = document.getElementById('tbody');
     
     // Filtra y ordena los datos de los 10 primeros asteroides
     const asteroides = datos.data.slice(0, 10).sort((a, b) => a.sat.prov_year - b.sat.prov_year);
 
-    // Obtener los nombres y años de descubrimiento
-    const labels = asteroides.map(asteroide => asteroide.sat.sat_fullname);
-    const years = asteroides.map(asteroide => asteroide.sat.prov_year);
+    // Tratar los valores nulos en sat_fullname
+    asteroides.forEach(asteroide => {
+      if (!asteroide.sat.sat_fullname) {
+        asteroide.sat.sat_fullname = 'El nombre es null para este asteroide';
+      }
+    });
 
-    // Crear un conjunto de datos para la gráfica de donut
-    const datasets = [{
-      data: years,
-      backgroundColor: ['#FF5733', '#33FFB1', '#336BFF', '#34FEA9', '#C433FF', '#CF5555','#CF4111'], // Colores para los años
-    }];
+    // Agrupar los años y contar cuántas veces aparece cada año, se utiliza el toString para unificar ya que la API de la nasa
+    // hay valores que estan numericos y otros como string
+    const yearCounts = {};
+    asteroides.forEach(asteroide => {
+      const year = asteroide.sat.prov_year.toString();
+      yearCounts[year] = (yearCounts[year] || 0) + 1;
+    });
+
+    // Obtener las etiquetas (años) y los valores (conteo) para el gráfico de donut
+    const labels = Object.keys(yearCounts);
+    const data = Object.values(yearCounts);
+
+    // Colores para los años
+    const backgroundColors = ['#FF5733', '#33FFB1', '#336BFF', '#34AEA9', '#C433FF', '#CF5555', '#CF4111'];
+
+    // Crear un conjunto de datos para el gráfico de donut
+    const dataset = {
+      data: data,
+      backgroundColor: backgroundColors,
+    };
 
     // Creación de gráfica
     new Chart(ctx, {
       type: 'doughnut',
       data: {
         labels: labels,
-        datasets: datasets,
+        datasets: [dataset],
       },
     });
 
@@ -58,7 +84,7 @@ fetch("https://ssd-api.jpl.nasa.gov/sb_sat.api", {
       tr.innerHTML = `
         <td class="border rounded-md border-slate-300 text-white bg-black px-4">${index + 1}</td>
         <td class="border rounded-md border-slate-300 text-white bg-black px-4">${asteroide.sat.sat_fullname}</td>
-        <td class="border rounded-md border-slate-300 text-white bg-black px-4">${asteroide.sat.prov_year}</td>
+        <td class="border rounded-md border-slate-300 text-white text-center bg-black px-4">${asteroide.sat.prov_year}</td>
       `;
       tbody.appendChild(tr);
     });
